cd5e69bddf66501e5dd049ca1fe412ea0c1c5a5a
clivejj <cjohnston1@stuy.edu>
2018-05-18 13:44:13 -0400

    update weather

--- utils/weather.py
+++ utils/weather.py
@@ -5,10 +5,11 @@ key = "901db038817e40bc4789ce2fc1dde133"
 
 def weekly():
     url = "https://api.darksky.net/forecast/" + key + "/40.730610,-73.935242?exclude=hourly,currently,minutely,alerts"
-    print url
+    #print url
     data = requests.get(url).json()
     data = data['daily']['data']
-    d = {x["time"] : {"min" : x["temperatureLow"], "max" : x["temperatureHigh"]} for x in data}
+    #print data
+    d = {x["time"] : {"min" : x["temperatureLow"], "max" : x["temperatureHigh"], 'desc' : x["icon"]} for x in data}
     print d
     
     return
