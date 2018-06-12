c810e9ee5456f3282183325667d1f32322abaf22
Manahal Tabassum <mtabassum@stuy.edu>
2018-06-12 15:34:39 -0400

    bar graph

--- dbb/static/graph.js
+++ dbb/static/graph.js
@@ -7,6 +7,10 @@ var getData = function(){
 	    var stats = JSON.parse(d);
 	    console.log(stats);
 	    var type = ['Tops','Bottoms','Shoes']
+	    var info = new Array(stats[0] + " " + type[0],
+				 stats[1] + " " + type[1],
+				 stats[2] + " " + type[2]);
+	    console.log(info);
 	    var chart = d3.select(".chart");
 	    var bar = chart.selectAll("div");
 	    var barUpdate = bar.data(stats);
@@ -14,7 +18,7 @@ var getData = function(){
 
 	    barEnter.transition().duration(2000).style("width", function(d){
 		return d * 50 + "px";});
-	    barEnter.data(type);
+	    barEnter.data(info);
 	    barEnter.text(function(d) {return d;});
 
 	}
